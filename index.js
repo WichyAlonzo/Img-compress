38e021e543e737eff9fb7d2aed498dc73888134e
WichyAlonzo <wishy.alonzo@gmail.com>
2025-01-08 13:22:29 -0800

    se agrega procentaje y el conteo de las imgenes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const sharp = require('sharp');
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
 
 const inputDir = path.join(__dirname, 'img');
 const outputDir = path.join(__dirname, 'output');
@@ -13,32 +14,50 @@ async function compressImage(inputPath, outputPath, maxSizeMB = 3) {
     try {
         const maxSizeBytes = maxSizeMB * 1024 * 1024;
         let quality = 100;
-        let buffer = await sharp(inputPath).jpeg({ quality }).toBuffer();
-        
-        // Reduce quality until the image is below the max size
+
+        let buffer = await sharp(inputPath)
+            .jpeg({ quality })
+            .toBuffer();
+
         while (buffer.length > maxSizeBytes && quality > 10) {
             quality -= 10;
-            buffer = await sharp(inputPath).jpeg({ quality }).toBuffer();
+            buffer = await sharp(inputPath)
+                .jpeg({ quality })
+                .toBuffer();
         }
-        
+
         fs.writeFileSync(outputPath, buffer);
-        console.log(`Image compressed and saved to ${outputPath}`);
+        return true;
     } catch (error) {
-        console.error('Error compressing image:', error);
+        console.error('Error al comprimir la imagen:', error);
+        return false;
     }
 }
 
 async function compressImagesInFolder(inputDir, outputDir) {
     const files = fs.readdirSync(inputDir);
+    const totalImages = files.filter(file => /\.(jpg|jpeg|png)$/i.test(file)).length;
+    let compressedCount = 0;
+
+    console.log(`Total de imágenes: ${totalImages}`);
+
     for (const file of files) {
         const inputPath = path.join(inputDir, file);
         const outputPath = path.join(outputDir, file);
 
         if (fs.lstatSync(inputPath).isFile() && /\.(jpg|jpeg|png)$/i.test(file)) {
-            await compressImage(inputPath, outputPath);
+            const result = await compressImage(inputPath, outputPath);
+            if (result) {
+                compressedCount++;
+                const progress = Math.round((compressedCount / totalImages) * 100);
+                readline.cursorTo(process.stdout, 0);
+                process.stdout.write(`Comprimiendo imágenes... ${progress}% (${compressedCount}/${totalImages})`);
+            }
         }
     }
+
+    console.log('\nProceso de compresión finalizado.');
 }
 
-// Compress all images in the 'img' folder and save them to the 'output' folder
+// Comprime todas las imágenes en la carpeta 'img' y las guarda en la carpeta 'output'
 compressImagesInFolder(inputDir, outputDir);
